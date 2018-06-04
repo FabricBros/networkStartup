a03d54ed650cdfb09c9b63c655546e665d7a5550
Huy Tran <huytran@Huys-MacBook-Pro.local>
2018-06-04 14:24:44 -0500

    added to toRunCC.sh

--- networkup/docker/toRunCC.sh
+++ networkup/docker/toRunCC.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-
+#inside cc directory
+# go build && CORE_CHAINCODE_LOGLEVEL=debug CORE_PEER_ADDRESS=127.0.0.1:7052 CORE_CHAINCODE_ID_NAME=name:version ./pathToCCBinary
 
 CORE_PEER_LOCALMSPID=Org1MSP CORE_PEER_MSPCONFIGPATH=/Users/huytran/MyCoolProjects/blockchain/coolnetwork/networkup/docker/crypto/v1.1/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/ peer channel create -o 127.0.0.1:7050 -c foo -f /Users/huytran/MyCoolProjects/blockchain/coolnetwork/networkup/docker/crypto/v1.1/foo.tx
 
