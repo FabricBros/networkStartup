a18294dd7edb2e1502cb3622273f492df6a73f91
Huy Tran <httran8884@gmail.com>
2018-10-07 15:00:02 -0400

    init and join channel success

--- networkup/docker/scripts/initChannels.sh
+++ networkup/docker/scripts/initChannels.sh
@@ -2,17 +2,16 @@
 # Copyright London Stock Exchange Group All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-set -e
+set -x
 # This script expedites the chaincode development process by automating the
 # requisite channel create/join commands
 pwd
 # We use a pre-generated orderer.block and channel transaction artifact (myc.tx),
 # both of which are created using the configtxgen tool
-
 # first we create the channel against the specified configuration in myc.tx
 # this call returns a channel configuration block - myc.block - to the CLI container
 echo "creating channel"
-peer channel create -c foo -f ./crypto/v1.3/foo.tx -o orderer0.example.com:7050
+peer channel create -t 60 -c foo -f ./crypto/v1.3/foo.tx -o orderer0.example.com:7050
 
 # now we will join the channel and start the chain with myc.block serving as the
 # channel's first block (i.e. the genesis block)
@@ -26,5 +25,4 @@ peer channel join -b foo.block
 
 #we should have bailed if above commands failed.
 #we are here, so they worked
-sleep 600000
 exit 0
